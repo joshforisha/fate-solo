5318546d45933a7f6c7fc65d0d6ba5ed41580f92
Josh Forisha <josh@forisha.com>
2020-09-11 15:01:58 -0500

    Include Journal component

---
+++ src/view/journal.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+`;
+
+export function Journal(): React.FC {
+  return <Container>Game story text goes here</Container>;
+}
